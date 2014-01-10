eccdf48130a09e46b14d743d4d9e4148e30e5348
Josh Taylor <joshrtay@gmail.com>
2014-01-10 12:30:28 -0800

    update bower and component packages

--- bower/inherits/inherits.js
+++
@@ -1,8 +0,0 @@
-var inherits = function (Child, Parent) {
-    for (var i in Parent.prototype) {
-        if (Child.prototype[i]) {
-            continue;
-        }
-        Child.prototype[i] = Parent.prototype[i];
-    }
-};
